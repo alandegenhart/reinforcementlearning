44b9d9b611374dbb2912cf235ebb3f6a849d5a83
Alan Degenhart <alandegenhart@gmail.com>
2020-01-31 08:50:21 -0500

    Adding base TDControl class

--- Discrete.py
+++ Discrete.py
@@ -1,10 +1,9 @@
 # Import libraries
 import numpy as np
-import gym
 
 
-class OnPolicyTDControl:
-	"""On-policy temporal difference (TD) control (aka Sarsa)."""
+class TDControl:
+	"""Base class for temporal difference (TD) control."""
 
 	def __init__(self, env):
 		# Add environment to the class and determine the size of the action and
@@ -16,7 +15,6 @@ class OnPolicyTDControl:
 		# Initialize policy and action values
 		self.P = np.ones([self.n_states, self.n_actions]) / self.n_actions
 		self.Q = np.zeros([self.n_states, self.n_actions])
-
 
 	def run(self, episodes,
 		block_size=100,
@@ -39,29 +37,22 @@ class OnPolicyTDControl:
 
 		# Iterate over episodes
 		for i in range(episodes):
-			# Step through an episode
-			s = self.env.reset()
-			a = self.sample_action(s)
+			
+			# Perform initialization procedures
+			self.s = self.env.reset()
+			self.episode_init()
+
+			# Iterate over steps in the episode
 			for t in range(max_episode_steps):
-				# Take action
-				s_next, r, done, info = self.env.step(a)
 
-				# Choose next action based on the current policy
-				a_next = self.sample_action(s_next)
-
-				# Update action value and policy
-				self.update_action_value(s, a, r, s_next, a_next)
-				self.update_policy(s)
-
-				# Update current state and action
-				a = a_next
-				s = s_next
+				# Perform operations for current step
+				done = self.episode_step()
 
 				# Check if the state is terminal
 				if done:
 					# Keep track of the outcome of the episode
 					self.episode_steps[i] = t + 1
-					self.episode_reward[i] = r  # Note this is the last reward
+					self.episode_reward[i] = self.r  # Outcome of episode
 					break
 
 			# Take any actions at the end of an episode (display status, etc.)
@@ -69,31 +60,30 @@ class OnPolicyTDControl:
 
 		return self.episode_steps, self.episode_reward
 
+	def episode_init(self):
+		"""Perform initialization operations for an episode."""
+
+	def episode_step(self):
+		"""Perform operations for a single episode step.
+		
+		This is an empty method for the base class. It is expected that this
+		will be over-ridden by child classes.
+
+		Returns:
+		:done 	Boolean indicate whether a terminal state was reached
+		"""
+		return False
 
 	def sample_action(self, s):
 		"""Sample an action from the current policy."""
 		return np.random.choice(range(self.n_actions), p=self.P[s, :])
 
-
-	def update_action_value(self, s, a, r, s_next, a_next):
+	def update_action_value(self):
 		"""Update action values.
 		
-		Inputs:
-		-- s -- state at time t
-		-- a -- action taken at time t
-		-- r -- reward received from taking action a
-		-- s_next -- new state after taking action a
-		-- a_next -- the next action according to the current policy
-
-		Currently this method implements Sarsa. Eventually this method will be
-		overridden by child classes implementing other methods (e.g.,
-		Q-learning, expected sarsa, etc.).
+		This is an empty method for the base class. It is expected that this
+		will be over-ridden by child classes.
 		"""
-
-		# Calculate target, update action value
-		targ = r + self.gamma * self.Q[s_next, a_next] - self.Q[s, a]
-		self.Q[s, a] = self.Q[s, a] + self.alpha * targ
-
 
 	def update_policy(self, s):
 		"""Epsilon-greedy policy update given current action values."""
@@ -101,10 +91,10 @@ class OnPolicyTDControl:
 		self.P[s, mask] = 1 - self.eps + (self.eps/self.n_actions)
 		self.P[s, ~mask] = self.eps/self.n_actions
 
-
 	def episode_finished(self, episode):
 		"""Take actions at the end of an episode."""
-		# Display a status update each block
+
+		# Display a status update at the end of each block
 		if (episode + 1) % self.block_size == 0:
 			# Calculate reward rate over the last block_size episode_steps
 			reward_rate = sum(self.episode_reward[episode - self.block_size + 1:
@@ -113,16 +103,106 @@ class OnPolicyTDControl:
 			print('Episode {} finished: reward rate = {}'.format(episode + 1, 
 				reward_rate))
 
+	def info(self):
+		"""Test print function for testing inheritance"""
+		print('Class: TDControl (base class)')
 
 
-""" To-do:
-- it might be helpful to add methods to initialize an episode and to take a
-step in an episode. This would make it easier to create child classes that
-implement slightly different algorithms (Q-learning for example)
-- plotting of results/action values?
-- function to save checkpoint
-- some way to schedule epsilon
-"""
+class Sarsa(TDControl):
+	"""On-policy temporal difference control (Sarsa).
+	
+	"""
+
+	def episode_init(self):
+		"""Perform initialization operations for an episode.
+
+		For Sarsa, we need to sample an action at the start of an episode. This
+		is needed b/c it is simpler if the main iteration for an episode samples
+		actions at the end of each step, meaning that we need to start with an
+		initial value before we step into the loop.
+		"""
+		self.a = self.sample_action(self.s)
+
+	def episode_step(self):
+		"""Perform operations for a single episode step."""
+		# Take action
+		self.s_next, self.r, done, info = self.env.step(self.a)
+
+		# Choose next action based on the current policy
+		self.a_next = self.sample_action(self.s_next)
+
+		# Update action value and policy
+		self.update_action_value()
+		self.update_policy(self.s)
+
+		# Update current state and action
+		self.a = self.a_next
+		self.s = self.s_next
+
+		return done
+
+	def update_action_value(self):
+		"""Update action values.
+
+		"""
+	
+		# Calculate target, update action value
+		targ = (self.r + self.gamma * self.Q[self.s_next, self.a_next] - 
+			self.Q[self.s, self.a])
+		self.Q[self.s, self.a] = self.Q[self.s, self.a] + self.alpha * targ
 
 
+	def info(self):
+		"""Test print function for testing inheritance"""
+		print('Class: Sarsa')
+
+
+class QLearning(TDControl):
+	"""Off-policy TD control (Q-learning)
+
+	The algorithm for Q-learning is very similar to that of Sarsa but with a few
+	exceptions:
+	- The action value update is slightly different
+	- Because the action value update for Sarsa uses the value of the next
+	action selected, it makes sense to do this after taking the action each
+	step. For Q-learning, it makes more sense to select the action before
+	"""
+
+	def episode_step(self):
+		"""Perform operations for a single episode step.
+
+		The algorithm for Sarsa assumes that we have already selected an action
+		as part of the previous step. This is done because the action value
+		update uses this action, so it is most appropriate just to keep the
+		selected action.
+		"""
+		# Take action
+		self.s_next, self.r, done, info = self.env.step(self.a)
+
+		# Choose next action based on the current policy
+		self.a_next = self.sample_action(self.s_next)
+
+		# Update action value and policy
+		self.update_action_value()
+		self.update_policy(self.s)
+
+		# Update current state and action
+		self.a = self.a_next
+		self.s = self.s_next
+
+		return done
+
+	def update_action_value(self):
+		"""Update action values.
+
+		"""
+	
+		# Calculate target, update action value
+		targ = (self.r + self.gamma * self.Q[self.s_next, self.a_next] - 
+			self.Q[self.s, self.a])
+		self.Q[self.s, self.a] = self.Q[self.s, self.a] + self.alpha * targ
+
+	def info(self):
+		"""Test print function for testing inheritance"""
+		print('Class: Q-learning')
 
